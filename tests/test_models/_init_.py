c90ade46886cdc7f33a216139d6c0c4aa51602c7
Ifeanyit103 <ifeanyit103@gmail.com>
2023-03-12 19:31:07 +0000

    AirBnB test Models

---
+++ tests/test_models/_init_.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""
+Created on 12th March, 2023.
+
+@author: Ifeanyichukwu Onuzulike
+"""
---
+++ tests/test_models/test_engine/_init_.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""
+Created on 12th March, 2023
+
+@author: Ifeanyichukwu Onuzulike
+"""
+
