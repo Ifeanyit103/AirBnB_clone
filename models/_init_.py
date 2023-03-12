58ad51df84c9771613cebc0914e9e96e9b3f10f1
Ifeanyit103 <ifeanyit103@gmail.com>
2023-03-12 14:40:07 +0000

    AirBnB models

---
+++ models/_init_.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Created on Fri. 10th March, 2023.
+
+@author: Ifeanchukwu Onuzulike
+"""
+from models.engine.file_storage import FileStorage
+
+
+storage = FileStorage()
+storage.reload()
